be249154a13d630b05a371d0e3ca279cfb2141db
Azikzz1 <aziretshaipov@gmail.com>
2025-01-20 20:03:25 +0600

    Home Work5

---
+++ posts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from posts.models import Post, Category, Tag
+
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "rate", "created_at"]
+    list_filter = ["category", "tags"]
+    search_fields = ["title", "description", "tags_name"]
+
+
+admin.site.register(Category)
+admin.site.register(Tag)
---
+++ posts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, HttpResponse, redirect
+import random
+from posts.models import Post
+from posts.forms import PostCreateForm
+from django.contrib.auth.decorators import login_required
+
+
+def main_view(request):
+    if request.method == "GET":
+        return render(request, "main.html")
+    else:
+        pass
+
+
+@login_required(login_url="/login/")
+def posts_list_view(request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(
+            request,
+            "posts/posts_list.html",
+            context={"posts": posts}
+        )
+
+
+@login_required(login_url="/login/")
+def post_detail_view(request, post_id):
+    if request.method == "GET":
+        post = Post.objects.get(id=post_id)
+        return render(request, "posts/post_detail.html", context={"post": post})
+
+
+@login_required(login_url="/login/")
+def post_create_view(request):
+    if request.method == "GET":
+        form = PostCreateForm()
+        return render(request, "posts/post_create.html", context={"form": form})
+    if request.method == "POST":
+        form = PostCreateForm(request.POST, request.FILES)
+        if not form.is_valid():
+            return render(request, "posts/post_create.html", context={"form": form})
+        elif form.is_valid():
+            form.save()
+            return redirect("/posts/")
